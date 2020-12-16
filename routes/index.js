63e75da59caac7150b0233169b5b0d1f033da6dc
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-12-16 11:37:43 -0300

    aplicação com qualidade pronto para automação CI CD

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const Cliente = require('../models/cliente');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var cliente = new Cliente(null, null, req.query.cpf)
+  let cpfValidado = cliente.validarCPF()
+  res.render('index', {cpfValidado:cpfValidado, cpf:req.query.cpf });
 });
 
 module.exports = router;
--- spec/cliente.spec.js
+++ spec/cliente.spec.js
@@ -6,3 +6,23 @@ test('Propriedades da classe de cliente', () => {
   expect(cliente.nome).toBe("Danilo");
   expect(cliente.cpf).toBe("418.825.350-90");
 });
+
+test('Dado que eu tenho um CPF válido, o mesmo precisa dar certo', () => {
+  let cliente = new Cliente(1, "Danilo",  "418.825.350-90")
+  expect(cliente.validarCPF()).toBe(true);
+});
+
+test('Dado que eu tenho um CPF inválido, o mesmo precisa dar errado', () => {
+  let cliente = new Cliente(1, "Danilo",  "418.825.350-91")
+  expect(cliente.validarCPF()).toBe(false);
+});
+
+test('Testando CPF 11111111111', () => {
+  let cliente = new Cliente(1, "Danilo",  "11111111111")
+  expect(cliente.validarCPF()).toBe(false);
+});
+
+test('Testando CPF 111111111111', () => {
+  let cliente = new Cliente(1, "Danilo",  "111111111111")
+  expect(cliente.validarCPF()).toBe(false);
+});
